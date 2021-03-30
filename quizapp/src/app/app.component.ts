858c09b20cd71e37a811c13d341dbb44eec80a04
pruthviraj <pruthvirajmandala@gmail.com>
2021-03-30 11:33:59 -0500

    Online quiz having result calculating issue

--- quizapp/src/app/app.component.ts
+++ quizapp/src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   flag:boolean = false;
-  b1:string = "Show Quiz";
+  b1:string = "Show test";
   ngOnInit(): void {
 
   }
   
   open(){
     this.flag = !this.flag;
-    if (this.flag) {this.b1 = "Close Quiz";}
-    else {this.b1 = "Show Quiz";}
+    if (this.flag) {this.b1 = "Close test";}
+    else {this.b1 = "Show test";}
   }
 }
--- quizapp/src/app/test/test.component.ts
+++ quizapp/src/app/test/test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { test}  from '../test.module';
+import {TestService} from '../test.service'
 
 @Component({
   selector: 'app-test',
@@ -7,9 +9,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  questions:Array<test>=[];
+  answerKey:Array<QA>=[];
+  userAnswers:Array<QA>=[];
+  numCorrect:number = 0;
+  resultFlag:boolean = false;
+  click:boolean = false;
+  msg:string=" ";
+  constructor(public quizQA:TestService) { }
 
   ngOnInit(): void {
+    this.quizQA.loadtestDetails().subscribe(result=>{this.questions=result
+    for (let i=0;i<10;i++){ 
+      this.createAnswerKey(result[i].question,result[i].correctAnswer)
+    }});
+    
+  }
+  onSelect(question:string,answer:number){
+    let obj = new QA(question,answer);
+    let n:boolean = true;
+    for (let i=0;i<this.userAnswers.length;i++){
+      if(this.userAnswers[i].question == question){
+        n = false;
+        this.userAnswers[i].answer = answer;
+      }
+    }
+    if(n){this.userAnswers.push(obj);} 
+
+  }
+  createAnswerKey(question:string, answer:number){
+    let obj = new QA(question,answer);
+    this.answerKey.push(obj);
+  }
+  onSubmit(){
+    this.compareAnswers();
+    this.resultFlag = true;
+    this.click = !this.click;
+    this.highlightAnswers();
+  }
+  compareAnswers(){
+    for(let j=0;j<10;j++){
+      let question = this.answerKey[j].question;
+      for (let i=0;i<10;i++){
+        if(this.userAnswers[i].question == question){
+          if(this.userAnswers[i].answer == this.answerKey[j].answer){
+            this.numCorrect++;
+          }
+          break;
+        }
+      }
+    }
+  }
+  resultColor():string{
+    let color:string = "red"
+    this.msg="You have not cleared the Test, Retake the test :)";
+    if(this.numCorrect>=5){
+      color = "green"
+      this.msg="You have cleared the test :)"
+    }
+    return color;
   }
 
+  highlightAnswers(){
+    for(let i=0;i<10;i++){
+      document.getElementById(this.userAnswers[i].question+this.userAnswers[i].answer)!.style.backgroundColor = "yellow";
+      document.getElementById(this.answerKey[i].question+this.answerKey[i].answer)!.style.backgroundColor = "green";
+    }
+  
+  }
+}
+class QA {
+  constructor(public question:string, public answer:number){}
 }
